7ea437b2ae35cd03cb90e93dcb1e0d094eb75db8
Lim Xuan Hui <limxuanhui1717@gmail.com>
2022-06-20 03:28:36 +0800

    WaitAWhile exercise

---
+++ basics_java/src/exercises/flowcontrols/whiles/WaitAWhile.java
@@ -0,0 +1,18 @@
+package exercises.flowcontrols.whiles;
+/* Created by limxuanhui on 20/6/22 */
+
+public class WaitAWhile {
+    public static void main(String[] args) {
+        int timeNow = 5;
+        int bedTime = 10;
+
+        while (timeNow < bedTime) {
+            System.out.println("It's only " + timeNow + " o'clock!");
+            System.out.println("I think I'll stay up just a liiiiittle longer....");
+            timeNow++; // Time passes
+        }
+
+        System.out.println("Oh. It's " + timeNow + " o'clock.");
+        System.out.println("Guess I should go to bed ...");
+    }
+}
