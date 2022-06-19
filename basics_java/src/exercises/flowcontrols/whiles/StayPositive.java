24e56673c7c3b929f4ca0a63c721eb01837d2ccf
Lim Xuan Hui <limxuanhui1717@gmail.com>
2022-06-20 03:38:15 +0800

    StayPositive exercise

--- basics_java/src/exercises/flowcontrols/whiles/StayPositive.java
+++ basics_java/src/exercises/flowcontrols/whiles/StayPositive.java
@@ -1,12 +1,21 @@
 package exercises.flowcontrols.whiles;
 /* Created by limxuanhui on 20/6/22 */
 
+import java.util.Scanner;
+
 public class StayPositive {
     public static void main(String[] args) {
-        int startNum = 10;
-        System.out.println("Counting down...");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What number should I count down from?");
+        int startNum = scanner.nextInt();
+        System.out.println("\nHere goes!\n");
+
         while (startNum >= 0) {
-            System.out.println(startNum--);
+            for (int i = 0; i < 10; i++) {
+                System.out.print(startNum-- + " ");
+                if (startNum < 0) break;
+            }
+            System.out.println();
         }
         System.out.println("\nBlast off!");
     }
