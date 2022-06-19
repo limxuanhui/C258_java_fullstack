26120392f18e3082e47bcdb33082f867eb0569f8
Lim Xuan Hui <limxuanhui1717@gmail.com>
2022-06-20 03:02:34 +0800

    Opinionator exercise

---
+++ basics_java/src/exercises/flowcontrols/random/Opinionator.java
@@ -0,0 +1,41 @@
+package exercises.flowcontrols.random;
+/* Created by limxuanhui on 20/6/22 */
+
+import java.util.Random;
+
+public class Opinionator {
+
+    public static void main(String[] args) {
+        Random randomizer = new Random();
+        System.out.println("I can't decide what animal I like the best.");
+        System.out.println("I know! Random can decide FOR ME!");
+
+        int x = randomizer.nextInt(5);
+
+        System.out.println("The number we chose was: " + x);
+
+        switch (x) {
+            case 0:
+                System.out.println("Llamas are the best!");
+                break;
+            case 1:
+                System.out.println("Dodos are the best!");
+                break;
+            case 2:
+                System.out.println("Woolly mammoths are DEFINITELY the best!");
+                break;
+            case 3:
+                System.out.println("Sharks are the greatest, they have their own week!");
+                break;
+            case 4:
+                System.out.println("Cockatoos are just so awesomme!");
+                break;
+            case 5:
+                // Never reached because randomizer randomize from 0 to 4 (inclusive)
+                System.out.println("Have you ever met a naked mole-rat? They're GREAT!");
+                break;
+        }
+
+        System.out.println("Thanks Random, maybe YOU'RE the best!");
+    }
+}
