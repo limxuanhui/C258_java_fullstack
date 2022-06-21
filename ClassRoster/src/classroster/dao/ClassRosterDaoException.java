c3f299de2e239d815eeb75db69c5d660b4d9ac84
Lim Xuan Hui <limxuanhui1717@gmail.com>
2022-06-21 14:37:50 +0800

    Created ClassRosterDaoException application-specific class

---
+++ ClassRoster/src/classroster/dao/ClassRosterDaoException.java
@@ -0,0 +1,12 @@
+package classroster.dao;
+/* Created by limxuanhui on 21/6/22 */
+
+public class ClassRosterDaoException extends Exception {
+    public ClassRosterDaoException(String message) {
+        super(message);
+    }
+
+    public ClassRosterDaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
